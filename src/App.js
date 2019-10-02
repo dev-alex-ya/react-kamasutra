74b6e6d64b465853af26a7cd923584066ea6b2ab
Alexandr Yarymokha <ya.alexandr.86@gmail.com>
2019-10-02 08:45:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <img src="https://www.freelogodesign.org/Content/img/logo-ex-7.png"></img>
+      </header>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+      </nav>
+      <div>Main content</div>
+    </div>
+  );
+}
+
+export default App;
