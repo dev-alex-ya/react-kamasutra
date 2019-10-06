6cd9854fd90c1f8fb1731d3da0074d56f6e75956
Alexandr Yarymokha <ya.alexandr.86@gmail.com>
2019-10-06 22:55:40 +0300

    ref Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import classes from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+        </div>
+    );
+};
 
+const Message = (props) => {
+    return(
+        <div className={classes.message}>
+            {props.message}
+        </div>
+    );
+};
 
 
 const Dialogs = (props) => {
@@ -10,29 +24,19 @@ const Dialogs = (props) => {
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
 
-                <div className={classes.dialog + ' ' + classes.active}>
-                    <NavLink to='/dialogs/1'>Стив Роджерс</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/3'>Брюс Брэннер</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/4'>Наташа Романофф</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/5'>Тор</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/6'>Доктор Стивен Стрэндж</NavLink>
-                </div>
-                <div className={classes.dialog}>
-                    <NavLink to='/dialogs/7'>Грут</NavLink>
-                </div>
+                <DialogItem name="Стив Роджерс" id='1' />
+                <DialogItem name="Энтони Старк" id='2' />
+                <DialogItem name="Брюс Брэннер" id='3' />
+                <DialogItem name="Наташа Романофф" id='4' />
+                <DialogItem name="Тор" id='5' />
+                <DialogItem name="Доктор Стивен Стрэндж" id='6' />
+                <DialogItem name="Грут" id='7' />
+
             </div>
             <div className={classes.messages}>
-                <div className={classes.message}>Hi</div>
-                <div className={classes.message}>Hey we are going to the cinema.</div>
-                <div className={classes.message}>are you coming with us</div>
+                <Message message="Hi"/>
+                <Message message="Hey we are going to the cinema."/>
+                <Message message="are you coming with us"/>
             </div>
         </div>
     );
