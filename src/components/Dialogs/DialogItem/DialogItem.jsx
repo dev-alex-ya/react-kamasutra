7084f93d8f61a14142d28fda84f906e25fdb4c97
Alexandr Yarymokha <ya.alexandr.86@gmail.com>
2019-10-08 11:02:16 +0300

    refactor styles in DialogItem

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import classes from "../Dialogs.module.css";
+import classes from "./DialogItem.module.css";
 import {NavLink} from "react-router-dom";
 
 const DialogItem = (props) => {
     return (
-        <div className={classes.dialog}>
-            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+        <div>
+            <NavLink activeClassName={classes.active} className={classes.dialogItem} to={`/dialogs/${props.id}`}>{props.name}</NavLink>
         </div>
     );
 };
