113047da83ca69c9e70986272dd0d2801423776b
AlexYa <ya.alexandr.86@gmail.com>
2019-11-18 23:32:52 +0200

    refactor: add constant strings instead of using strings

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST = 'UPDATE-NEW-POST';
+
 export let store = {
     _state: {
         messagesPage: {
@@ -46,7 +49,7 @@ export let store = {
     },
 
     dispatch(action) {
-        if(action.type === 'ADD-POST') {
+        if(action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -56,7 +59,7 @@ export let store = {
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
         }
-        else if(action.type === 'UPDATE-NEW-POST') {
+        else if(action.type === UPDATE_NEW_POST) {
             this._state.profilePage.newPostText = action.newText  ;
             this._callSubscriber(this._state);
         }
@@ -66,10 +69,10 @@ export let store = {
 
 
 export const addPostActionCreator = () => {
-    return ({type: 'ADD-POST'});
+    return ({type: ADD_POST});
 };
 export const updateNewPostTextActionCreator = (text) => {
-    return ({type: 'UPDATE-NEW-POST', newText: text});
+    return ({type: UPDATE_NEW_POST, newText: text});
 };
 
 // export default store;
